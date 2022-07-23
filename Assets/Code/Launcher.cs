99e402b89be32c20d096e3346aa406d48a8c71d6
AlekTananykin <70412225+AlekTananykin@users.noreply.github.com>
2022-07-24 01:14:44 +0300

    Lesson3 home 1 and 2  tasks

--- Assets/Code/Launcher.cs
+++ Assets/Code/Launcher.cs
@@ -1,5 +1,6 @@
 using Photon.Pun;
 using Photon.Realtime;
+using System.Collections;
 using UnityEngine;
 
 public class Launcher : MonoBehaviourPunCallbacks
@@ -15,6 +16,17 @@ public class Launcher : MonoBehaviourPunCallbacks
     void Start()
     {
         Connect();
+        Disconnect();
+
+        StartCoroutine(DisconnectCoroutine());
+    }
+
+    IEnumerator DisconnectCoroutine()
+    {
+        const float seconds = 60f; 
+        yield return new WaitForSeconds(seconds);
+
+        Disconnect();
     }
 
     private void Connect()
@@ -35,11 +47,42 @@ public class Launcher : MonoBehaviourPunCallbacks
         if (!PhotonNetwork.IsConnected)
             return;
 
-        
+        PhotonNetwork.Disconnect();
     }
 
     public override void OnConnectedToMaster()
     {
         Debug.Log("OnConnectedToMaster was called by PUN");
+        PhotonNetwork.JoinLobby();
     }
+
+    public override void OnJoinedLobby()
+    {
+        base.OnJoinedLobby();
+
+        Debug.Log($"joined lobby: {PhotonNetwork.InLobby}");
+        PhotonNetwork.JoinOrCreateRoom("theRoom", new RoomOptions 
+        { 
+            MaxPlayers  = 4, 
+            IsVisible = true
+        }, TypedLobby.Default);
+    }
+
+
+    public override void OnJoinedRoom()
+    {
+        base.OnJoinedRoom();
+        Debug.Log($"OnJoinedRoom: {PhotonNetwork.InRoom}");
+    }
+
+    public override void OnDisconnected(DisconnectCause cause)
+    {
+        base.OnDisconnected(cause);
+
+        Debug.Log(
+            $"Photon server is disconnected. Cause is: {cause.ToString()}");
+    }
+
+
+
 }
